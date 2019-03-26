a59c7bfe480179edb9df5fb2465abac17a750e6b
Michele Galvagno <michicello@gmail.com>
2019-03-26 18:41:11 +0100

    Everything works up to the passing of data between view controllers

--- Captioned/DetailViewController.swift
+++ Captioned/DetailViewController.swift
@@ -10,23 +10,53 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
-    var selectedImage: String?
+    var selectedPicture: Picture?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(editCaption))
+        
+        if let imageToLoad = selectedPicture?.image {
+            let path = getDocumentsDirectory().appendingPathComponent(imageToLoad)
+            imageView.image = UIImage(contentsOfFile: path.path)
+            print(imageToLoad)
+        } else {
+            print("Image not found!")
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
     }
-    */
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func editCaption() {
+        let captionAC = UIAlertController(title: "Add caption!", message: "Describe your picture", preferredStyle: .alert)
+        captionAC.addTextField { (textField) in
+            textField.placeholder = "Enter caption"
+            textField.autocapitalizationType = .sentences
+            textField.font = UIFont.systemFont(ofSize: 20.0)
+        }
+        
+        captionAC.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak self, weak captionAC] _ in
+            guard let newCaption = captionAC?.textFields?[0].text else { return }
+            
+            self?.title = newCaption
+            self?.selectedPicture?.caption = newCaption
+        }))
+        
+        present(captionAC, animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
+    }
 }
--- Captioned/ViewController.swift
+++ Captioned/ViewController.swift
@@ -8,33 +8,44 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: - Properties
-    var captions = [String]()
+    var pictures = [Picture]()
     
     // MARK: - Actions
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
-        
+        // setup of image picker
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            if let availableMediaTypes = UIImagePickerController.availableMediaTypes(for: .camera) {
+                picker.mediaTypes = availableMediaTypes
+                picker.sourceType = .camera
+            }
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        picker.delegate = self
+        present(picker, animated: true)
     }
     
     // MARK: - Views Management
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for i in 0...100 {
-            captions.append("\(i)")
-        }
-        
+        title = "Captioned"
+        load()
     }
     
     // MARK: - TableView Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return captions.count
+        return pictures.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Caption", for: indexPath)
-        cell.textLabel?.text = captions[indexPath.row]
+        cell.textLabel?.text = pictures[indexPath.row].image
+        cell.detailTextLabel?.text = pictures[indexPath.row].caption
         
         return cell
     }
@@ -42,13 +53,61 @@ class ViewController: UITableViewController {
     // MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let detailVC = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            detailVC.selectedImage = captions[indexPath.row]
-            detailVC.title = captions[indexPath.row]
+            detailVC.selectedPicture = pictures[indexPath.row]
+            detailVC.title = pictures[indexPath.row].caption
             
             navigationController?.pushViewController(detailVC, animated: true)
         }
     }
 
-
+    // MARK: - ImagePicker Delegate Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let picture = Picture(caption: "Caption", image: imageName)
+        pictures.append(picture)
+        save()
+        tableView.reloadData()
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Helper methods
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(pictures) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "pictures")
+        } else {
+            print("Failed to save the pictures")
+        }
+    }
+    
+    func load() {
+        let defaults = UserDefaults.standard
+        
+        if let savedPictures = defaults.object(forKey: "pictures") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                pictures = try jsonDecoder.decode([Picture].self, from: savedPictures)
+            } catch {
+                print("Failed to load pictures.")
+            }
+        }
+    }
 }
 
