241f02c192371e785833e06ab50cf1c54ebb4f8b
Michele Galvagno <michicello@gmail.com>
2019-03-26 12:06:07 +0100

    Basic functionalities setup.

---
+++ Captioned/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Captioned
+//
+//  Created by Michele Galvagno on 26/03/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Captioned/ViewController.swift
+++ Captioned/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    // MARK: - Properties
+    var captions = [String]()
+    
+    // MARK: - Actions
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    // MARK: - Views Management
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...100 {
+            captions.append("\(i)")
+        }
+        
+    }
+    
+    // MARK: - TableView Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return captions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Caption", for: indexPath)
+        cell.textLabel?.text = captions[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let detailVC = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            detailVC.selectedImage = captions[indexPath.row]
+            detailVC.title = captions[indexPath.row]
+            
+            navigationController?.pushViewController(detailVC, animated: true)
+        }
     }
 
 
